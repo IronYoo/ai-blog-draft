5eaa7cee19728c9aa76bb6e5183cf5374d817978
sceonn <yoou201@naver.com>
2024-08-28 08:12:40 +0900

    init

---
+++ src/test/kotlin/com/kotlin/aiblogdraftapi/AiBlogDraftApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin.aiblogdraftapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AiBlogDraftApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
