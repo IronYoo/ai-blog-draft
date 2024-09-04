0473f967d9ecccf1c905d72fac903d884b919586
sceonn <yoou201@naver.com>
2024-09-04 10:04:19 +0900

    chore: mocck 의존성 추가, test 환경 application.yml cloud 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
     testImplementation("io.kotest:kotest-assertions-core:5.6.2")
+    testImplementation("io.mockk:mockk:1.13.12")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
